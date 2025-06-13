6c28dfa5c434112fe17e08af0fe2f2f97431d8ff
EseSx <santiagoeseiza10@gmail.com>
2025-06-13 16:40:16 -0300

    probando 9

--- modulos/autos.py
+++ modulos/autos.py
@@ -37,14 +37,14 @@ def agregarAuto(data):
         conn.close()
 
 
-def borrarAuto(auto_id):
+def borrarAuto(data):
     """
     Elimina un auto de la tabla 'autos' por su ID.
     """
     conn = get_connection()
     cur = conn.cursor()
     try:
-        cur.execute("DELETE FROM autos WHERE auto_id = %s", (auto_id,))
+        cur.execute("DELETE FROM autos WHERE auto_id = %s", (data.auto_id,))
 
         return {"Mensaje": "Auto eliminado exitosamente"}
     except Exception as e:
