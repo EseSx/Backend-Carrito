1c7bec0dcb72d3f840afe595e97dcc5c7c510dc1
EseSx <santiagoeseiza10@gmail.com>
2025-06-16 01:52:46 -0300

    Añadi la validacion de usuario administrativo

--- encriptaRapido.py
+++
@@ -1,8 +0,0 @@
-import bcrypt
-
-contraseña = "contraseñaDeLaAdministracion"
-
-hashed = bcrypt.hashpw(contraseña.encode("utf-8"), bcrypt.gensalt())
-hash_string = hashed.decode()
-
-print(hash_string)
